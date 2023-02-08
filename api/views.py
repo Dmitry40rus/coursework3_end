ee80d24a464a9a5afceea7a779b413f392bc03a3
dmitrijizotov <dimon-izotov@mail.ru>
2023-02-08 05:43:41 +0300

    coursework_3_e

---
+++ api/views.py
@@ -0,0 +1,21 @@
+from flask import jsonify, Blueprint
+
+from dao.dao import PostDAO
+import logging, datetime
+
+
+api_blueprint = Blueprint('api_blueprint', __name__)
+posts = PostDAO('./data/posts.json', './data/comments.json')
+logging.basicConfig(filename='./logs/basic.log', level=logging.INFO)
+
+@api_blueprint.route('/api/posts/', methods=['GET'])
+def get_all_posts():
+    logging.info(f'{datetime.datetime.now()} [INFO] Запрос /api/posts/')
+    res = posts.load_posts_json()
+    return jsonify(posts.load_posts_json())
+
+
+@api_blueprint.route('/api/posts/<int:postid>', methods=['GET'])
+def get_post_by_id(postid):
+    logging.info(f'{datetime.datetime.now()} [INFO] Запрос /api/posts/{postid}')
+    return jsonify(posts.get_post_by_pk_json(postid))
